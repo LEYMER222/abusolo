1ab13fed01b7daa6f7b176ac3a0891981921d5c5
LEYMER222 <abdullohshaymanov222@gmail.com>
2025-03-26 13:12:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let ism = prompt("Ismingizni Yozing");
+let id = prompt("Mars Space Idiingizni kiriting")
+let pasword =prompt("Mars Space Passwordingizni kiriting")
+
+let hammasi = {
+    Ism:`${ism}`,
+    Id:`${id}`,
+    pasword:`${pasword}`
+}
+console.log(hammasi)
